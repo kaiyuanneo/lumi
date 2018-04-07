b9f5eec84cb74c306a6ba2ec601c110aebef3d3a
Kai Yuan Neo <kai@projectweforgot.com>
2018-04-07 18:11:44 +0800

    navbar
    Test Plan: works
    
    Differential Revision: https://projectweforgot.phacility.com/D184

--- website/src/containers/NavBarContainer.js
+++ website/src/containers/NavBarContainer.js
@@ -1,3 +1,4 @@
+// NB: Private functions are underscore-prefixed and exported for tests
 import * as firebase from 'firebase';
 import React from 'react';
 import { connect } from 'react-redux';
@@ -9,16 +10,16 @@ import * as constants from '../static/constants';
 
 
 const mapStateToProps = (state) => {
-  let groupIdLabel = state.home.groupId;
-  let groupNameLabel = state.home.groupName;
-  if (groupIdLabel) {
+  let groupIdLabel = null;
+  let groupNameLabel = null;
+  if (state.home.groupId) {
     groupIdLabel = (
       <Navbar.Text pullRight>
         {constants.NAVBAR_ITEM_GROUP_ID}{state.home.groupId}
       </Navbar.Text>
     );
   }
-  if (groupNameLabel) {
+  if (state.home.groupName) {
     groupNameLabel = ` ${state.home.groupName}`;
   }
   return {
@@ -28,27 +29,35 @@ const mapStateToProps = (state) => {
   };
 };
 
+
+export const _saveAuthUserGroupInfo = async (dispatch, activeGroupRef, activeGroupSnapshot) => {
+  const activeGroup = activeGroupSnapshot.val();
+  // If active group not set, auth user does not belong to a group yet
+  if (!activeGroup) {
+    return;
+  }
+  // Turn off this listener once auth user is in a group
+  activeGroupRef.off();
+  // Get group information from DB
+  const groupRef = firebase.database().ref(`${constants.DB_PATH_LUMI_GROUPS}/${activeGroup}`);
+  const groupSnapshot = await groupRef.once(constants.DB_EVENT_NAME_VALUE);
+  dispatch(actions.saveAuthUserGroupInfo(activeGroup, groupSnapshot.val().name));
+};
+
+
+export const _getGroupInfo = (dispatch) => {
+  // Populate Group ID and Group Name in navbar with auth user's group information
+  const db = firebase.database();
+  const authUid = firebase.auth().currentUser.uid;
+  const activeGroupRef = db.ref(`${constants.DB_PATH_USERS}/${authUid}/activeGroup`);
+  activeGroupRef.on(constants.DB_EVENT_NAME_VALUE, (activeGroupSnapshot) => {
+    _saveAuthUserGroupInfo(dispatch, activeGroupRef, activeGroupSnapshot);
+  });
+};
+
+
 const mapDispatchToProps = dispatch => ({
-  getGroupInfo: () => {
-    // Populate Group ID and Group Name in navbar with auth user's group information
-    const db = firebase.database();
-    const authUid = firebase.auth().currentUser.uid;
-    const activeGroupRef = db.ref(`${constants.DB_PATH_USERS}/${authUid}/activeGroup`);
-    activeGroupRef.on(constants.DB_EVENT_NAME_VALUE, (activeGroupSnapshot) => {
-      const activeGroup = activeGroupSnapshot.val();
-      // If active group not set, auth user does not belong to a group yet
-      if (!activeGroup) {
-        return;
-      }
-      // Turn off this listener once auth user is in a group
-      activeGroupRef.off();
-      // Get group information from DB
-      const groupRef = db.ref(`${constants.DB_PATH_LUMI_GROUPS}/${activeGroup}`);
-      groupRef.once(constants.DB_EVENT_NAME_VALUE, (groupSnapshot) => {
-        dispatch(actions.saveAuthUserGroupInfo(activeGroup, groupSnapshot.val().name));
-      });
-    });
-  },
+  getGroupInfo: () => _getGroupInfo(dispatch),
   switchProduct: (eventKey) => {
     // Clicking sign out will trigger this because it is a child of the navbar
     if (eventKey === constants.PRODUCT_CODE_SIGN_OUT) {
@@ -59,12 +68,14 @@ const mapDispatchToProps = dispatch => ({
   },
 });
 
+
 const mergeProps = (stateProps, dispatchProps, ownProps) => ({
   ...stateProps,
   ...dispatchProps,
   ...ownProps,
   signOut: () => firebase.auth().signOut(),
 });
+
 
 const NavBarContainer = connect(
   mapStateToProps,

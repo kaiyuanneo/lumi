e7e637d7f30f0f50969e95cbe1aed0f44b3c498b
Kai Yuan Neo <kai@projectweforgot.com>
2018-04-25 17:07:15 +0800

    center bottom navbar
    Test Plan: lgtm
    
    Differential Revision: https://projectweforgot.phacility.com/D198

--- website/src/components/NavBottomComponent.js
+++ website/src/components/NavBottomComponent.js
@@ -8,26 +8,29 @@ import * as constants from '../static/constants';
 
 const NavBottomComponent = props => (
   <Navbar className="nav-bottom" fixedBottom onSelect={props.switchProduct}>
-    <Nav activeKey={props.activeKey}>
-      <NavItem eventKey={constants.PRODUCT_CODE_TIMELINE}>
-        <Flexbox flexDirection="column" alignItems="center">
-          <Glyphicon glyph="home" />
-          {constants.NAVBAR_ITEM_TIMELINE}
-        </Flexbox>
-      </NavItem>
-      <NavItem eventKey={constants.PRODUCT_CODE_CHAT}>
-        <Flexbox flexDirection="column" alignItems="center">
-          <Glyphicon glyph="plus" />
-          {constants.NAVBAR_ITEM_CHAT}
-        </Flexbox>
-      </NavItem>
-      <NavItem eventKey={constants.PRODUCT_CODE_SUMMARY}>
-        <Flexbox flexDirection="column" alignItems="center">
-          <Glyphicon glyph="info-sign" />
-          {constants.NAVBAR_ITEM_SUMMARY}
-        </Flexbox>
-      </NavItem>
-    </Nav>
+    {/* Not using space-between bc Flexbox btw Nav and NavItem breaks react-bootstrap style */}
+    <Flexbox justifyContent="center">
+      <Nav activeKey={props.activeKey}>
+        <NavItem eventKey={constants.PRODUCT_CODE_TIMELINE}>
+          <Flexbox flexDirection="column" alignItems="center">
+            <Glyphicon glyph="home" />
+            {constants.NAVBAR_ITEM_TIMELINE}
+          </Flexbox>
+        </NavItem>
+        <NavItem eventKey={constants.PRODUCT_CODE_CHAT}>
+          <Flexbox flexDirection="column" alignItems="center">
+            <Glyphicon glyph="plus" />
+            {constants.NAVBAR_ITEM_CHAT}
+          </Flexbox>
+        </NavItem>
+        <NavItem eventKey={constants.PRODUCT_CODE_SUMMARY}>
+          <Flexbox flexDirection="column" alignItems="center">
+            <Glyphicon glyph="info-sign" />
+            {constants.NAVBAR_ITEM_SUMMARY}
+          </Flexbox>
+        </NavItem>
+      </Nav>
+    </Flexbox>
   </Navbar>
 );
 

eb3b06ccd0c904b5ebe9e30c8e72e61c70ebae6f
Kai Yuan Neo <kai@projectweforgot.com>
2018-04-27 14:11:25 +0800

    standardise width of bottom nav icons, change care profile to summary
    Test Plan: works
    
    Differential Revision: https://projectweforgot.phacility.com/D206

--- website/src/components/NavBottomComponent.js
+++ website/src/components/NavBottomComponent.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import AddIcon from 'react-icons/lib/md/add';
 import HomeIcon from 'react-icons/lib/md/home';
-import PersonIcon from 'react-icons/lib/md/person';
+import StatsIcon from 'react-icons/lib/io/stats-bars';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
 
 import * as constants from '../static/constants';
@@ -14,7 +14,7 @@ const NavBottomComponent = props => (
     {/* Not using space-between bc Flexbox btw Nav and NavItem breaks react-bootstrap style */}
     <Flexbox justifyContent="center">
       <Nav activeKey={props.activeKey}>
-        <NavItem eventKey={constants.PRODUCT_CODE_TIMELINE}>
+        <NavItem eventKey={constants.PRODUCT_CODE_TIMELINE} className="nav-bottom-item">
           <Flexbox flexDirection="column" alignItems="center">
             <HomeIcon size={constants.NAVBAR_ICON_SIZE} />
             <div className="nav-bottom-text">
@@ -22,19 +22,19 @@ const NavBottomComponent = props => (
             </div>
           </Flexbox>
         </NavItem>
-        <NavItem eventKey={constants.PRODUCT_CODE_CHAT}>
+        <NavItem eventKey={constants.PRODUCT_CODE_CHAT} className="nav-bottom-item">
           <Flexbox flexDirection="column" alignItems="center">
             <AddIcon size={constants.NAVBAR_ICON_SIZE} />
             <div className="nav-bottom-text">
-              {constants.NAVBAR_ITEM_NEW_MEMORY}
+              {constants.NAVBAR_ITEM_ADD_MEMORY}
             </div>
           </Flexbox>
         </NavItem>
-        <NavItem eventKey={constants.PRODUCT_CODE_SUMMARY}>
+        <NavItem eventKey={constants.PRODUCT_CODE_SUMMARY} className="nav-bottom-item">
           <Flexbox flexDirection="column" alignItems="center">
-            <PersonIcon size={constants.NAVBAR_ICON_SIZE} />
+            <StatsIcon size={constants.NAVBAR_ICON_SIZE} />
             <div className="nav-bottom-text">
-              {constants.NAVBAR_ITEM_CARE_PROFILE}
+              {constants.NAVBAR_ITEM_SUMMARY}
             </div>
           </Flexbox>
         </NavItem>
